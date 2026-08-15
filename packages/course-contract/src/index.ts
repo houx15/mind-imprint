6dfbfdb071dc3ecdb3be7b6a3768b18d0f03dd8a
houyuxin <houyuxin1234@sina.cn>
2026-08-16 00:18:26 +0800

    feat(course-contract): layout, narration, navigation schemas

--- packages/course-contract/src/index.ts
+++ packages/course-contract/src/index.ts
@@ -1,3 +1,6 @@
 export const COURSE_CONTRACT_VERSION = "0.0.0";
 export * from "./primitives";
 export * from "./blocks";
+export * from "./layout";
+export * from "./narration";
+export * from "./navigation";
